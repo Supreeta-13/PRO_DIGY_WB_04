2aead7a190c798b1ea3cadbcc72026305822a71a
Supreeta-13 <supreeta4@gmail.com>
2024-09-06 18:18:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : [" |  Web Developer","|Custom Web Designer"," | UI/UX Designer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
